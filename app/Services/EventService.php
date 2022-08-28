30d8c603a55442b03aa91663aacf85fb54121557
Semeton <balogunsemeton@gmail.com>
2022-08-28 22:38:32 +0100

    Comments

--- app/Services/EventService.php
+++ app/Services/EventService.php
@@ -6,11 +6,18 @@ use App\Events\BadgeUnlocked;
 use App\Events\AchievementUnlocked;
 
 class EventService{
+    
+    /**
+     * Trigger event
+     */
     public function triggerAchievementEvent(string $achievementName, $user): ?AchievementUnlocked{
         $event = event(new AchievementUnlocked($achievementName, $user));
         return $event;
     }
 
+    /**
+     * Trigger event
+     */
     public function triggerBadgeEvent(string $badgeName, $user): ?BadgeUnlocked{
         $event = event(new BadgeUnlocked($badgeName, $user));
         return $event;
