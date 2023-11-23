3c11d0ce9d1a5ec8a89b0981535a61102b581cbc
Semeton <balogunsemeton@gmail.com>
2023-11-23 21:49:31 +0100

    Added explanatory comments

--- app/Abstracts/AbstractBadge.php
+++ app/Abstracts/AbstractBadge.php
@@ -6,17 +6,37 @@ use App\Models\User;
 use App\Models\Badge;
 
 abstract class AbstractBadge{
+    /**
+     * Check if a badge exists for a total number of achievements
+     *
+     * @param integer $totalAchievements
+     * @return Badge|null
+     */
     public function checkIfBadgeExists(int $totalAchievements): ?Badge {
         $badgeExist = Badge::where('achievement_score', $totalAchievements)->first();
         
         return $badgeExist;
     }
 
+    /**
+     * Check if a badge is already unlocked by a user
+     *
+     * @param string $badgeName
+     * @param User $user
+     * @return Badge|null
+     */
     public function checkIfBadgeAlreadyUnlockedByUser(string $badgeName, User $user): ?Badge {
         $exist = $user->badges()->where('name', $badgeName)->first();
         return $exist;
     }
 
+    /**
+     * Unlock a new badge for a user
+     *
+     * @param string $badgeName
+     * @param User $user
+     * @return Badge
+     */
     public function unlockNewBadge(string $badgeName, User $user): Badge {
         $badge = $user->badges()->create(['name' => $badgeName]);
         return $badge;
