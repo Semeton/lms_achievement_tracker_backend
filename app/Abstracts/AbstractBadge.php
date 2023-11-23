1ae07034b0b74475a18865d94c079a8977b1c831
Semeton <balogunsemeton@gmail.com>
2023-11-23 21:48:04 +0100

    Implementing strict typing

--- app/Abstracts/AbstractBadge.php
+++ app/Abstracts/AbstractBadge.php
@@ -6,18 +6,18 @@ use App\Models\User;
 use App\Models\Badge;
 
 abstract class AbstractBadge{
-    public function checkIfBadgeExists($totalAchievements){
+    public function checkIfBadgeExists(int $totalAchievements): ?Badge {
         $badgeExist = Badge::where('achievement_score', $totalAchievements)->first();
         
         return $badgeExist;
     }
 
-    public function checkIfBadgeAlreadyUnlockedByUser($badgeName, User $user){
+    public function checkIfBadgeAlreadyUnlockedByUser(string $badgeName, User $user): ?Badge {
         $exist = $user->badges()->where('name', $badgeName)->first();
         return $exist;
     }
 
-    public function unlockNewBadge($badgeName, User $user){
+    public function unlockNewBadge(string $badgeName, User $user): Badge {
         $badge = $user->badges()->create(['name' => $badgeName]);
         return $badge;
     }
