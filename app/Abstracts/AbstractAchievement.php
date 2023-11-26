796c237352bb8255518b8ae1490448149c583f03
Semeton <balogunsemeton@gmail.com>
2023-11-26 21:45:06 +0100

    Adding comment

--- app/Abstracts/AbstractAchievement.php
+++ app/Abstracts/AbstractAchievement.php
@@ -11,6 +11,14 @@ use App\Models\CommentAchievement;
 
 abstract class AbstractAchievement {
 
+    /**
+     * Unlock an achievement for a user and create a record of it in the database
+     *
+     * @param User $user
+     * @param string $achievementName
+     * @param string $achievementType
+     * @return UserAchievement
+     */
     public function unlockAchievement(User $user, string $achievementName, string $achievementType): UserAchievement {
         $achievement = $user->achievements()->firstOrCreate([
             'name' => $achievementName, 
@@ -20,11 +28,23 @@ abstract class AbstractAchievement {
         return $achievement;
     }
 
+    /**
+     * Check if a lesson achievement exist for a number of comment made by a usert
+     *
+     * @param integer $lessonCount
+     * @return LessonAchievement|null
+     */
     public function checkLessonAchievementExist(int $lessonCount): ?LessonAchievement {
         $exist = LessonAchievement::where('lessons', $lessonCount)->first();
         return $exist;
     }
 
+    /**
+     * Check if a comment achievement exist for a number of comment made by a usert
+     *
+     * @param integer $commentCount
+     * @return CommentAchievement|null
+     */
     public function checkCommentAchievementExist(int $commentCount): ?CommentAchievement {
         $exist = CommentAchievement::where('comments', $commentCount)->first();
         return $exist;
