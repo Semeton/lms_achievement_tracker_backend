e055dd4416f015772b5d5c92da44539b1b39d1cf
Semeton <balogunsemeton@gmail.com>
2023-11-26 21:39:13 +0100

    Ensuring strict typing

--- app/Abstracts/AbstractAchievement.php
+++ app/Abstracts/AbstractAchievement.php
@@ -1,14 +1,17 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Abstracts;
 
 use App\Models\User;
+use App\Models\UserAchievement;
 use App\Models\LessonAchievement;
 use App\Models\CommentAchievement;
 
 abstract class AbstractAchievement {
 
-    public function unlockAchievement(User $user, String $achievementName, String $achievementType){
+    public function unlockAchievement(User $user, string $achievementName, string $achievementType): UserAchievement {
         $achievement = $user->achievements()->firstOrCreate([
             'name' => $achievementName, 
             'achievement_type' => $achievementType
@@ -17,12 +20,12 @@ abstract class AbstractAchievement {
         return $achievement;
     }
 
-    public function checkLessonAchievementExist($lessonCount){
+    public function checkLessonAchievementExist(int $lessonCount): ?LessonAchievement {
         $exist = LessonAchievement::where('lessons', $lessonCount)->first();
         return $exist;
     }
 
-    public function checkCommentAchievementExist($commentCount){
+    public function checkCommentAchievementExist(int $commentCount): ?CommentAchievement {
         $exist = CommentAchievement::where('comments', $commentCount)->first();
         return $exist;
     }
